62c5a560a9e62281bc4fb830224c9c9c85193fb1
Twanivi <139391328+Twanivi@users.noreply.github.com>
2023-09-28 21:02:49 +0300

    Add files via upload

--- hw11/script.js
+++ hw11/script.js
@@ -1,24 +1,20 @@
-const img = document.querySelectorAll('img');
-let images = [];
+const wrapper = document.querySelector('.choice-img');
+const images = Array.from(document.querySelectorAll('.choice-img img'));
 
-
-if(localStorage.checkedImg){
-    images = JSON.parse(localStorage.checkedImg);
-    console.log(images);
-
+if(localStorage.activeSrc){
+    document.body.style.backgroundImage = `url(${localStorage.activeSrc})`; 
+} else{
+    document.body.style.backgroundImage = `url(${images[0].src})`;
 }
 
-
-document.addEventListener('click', (event) => {
-    event.preventDefault();
-    img.forEach(item => {
-    const src = item.getAttribute('src');
-    localStorage.chekedImg = JSON.stringify({item: 'src'});
-
-})
-    if(event.target = img){
-        document.body.style.backgroundImage = 'url(event.target)';
+wrapper.addEventListener('click', (e) => {
+    if(e.target.matches('img')){
+        const activeImage = e.target;  // img, на которую кликнули
+        images.forEach(img => img.classList.remove('active'));
+        activeImage.classList.add('active');
+        const activeSrc = activeImage.getAttribute('src');   // src картинки, на которую кликнули
+        localStorage.activeSrc = activeSrc
+        document.body.style.backgroundImage = `url(${activeSrc})`;
     }
 })
-
 
