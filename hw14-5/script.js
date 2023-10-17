dcfa3e518951430123dc28849e906c92b599f004
Twanivi <139391328+Twanivi@users.noreply.github.com>
2023-10-17 21:53:31 +0300

    Add files via upload

--- hw14-5/script.js
+++ hw14-5/script.js
@@ -5,15 +5,25 @@ const forecast = document.querySelector('#forecast');
 
 
 function renderMain(town, time, img, event, temp, speed){
+  setInterval(() => {
+    const date = new Date().toLocaleTimeString();
+    const time = document.querySelector('.time');
+    time.textContent = date;
+  }, 1000);
+
   current.insertAdjacentHTML('beforeend', `
   <p class="city">${town}</p>
   <p class="time">${time}</p>
-  <img src="${img}" alt="falldown">
-  <p class="falldown-text">${event}</p>
-  <p class="degree">${temp} ℃</p>
-  <div class="wind">
+  <div class="center-info">
+    <div class="img-falldown">
+     <img src="https://openweathermap.org/img/wn/${img}@2x.png" alt="falldown">
+    </div>
+    <p class="falldown-text">${event}</p>
+    <p class="degree">${temp.toFixed()} ℃</p>
+    <div class="wind">
       <p class="speed">Speed</p>
-      <p class="speed-number">${speed} m/s</p>
+      <p class="speed-number">${speed.toFixed(1)} m/s</p>
+    </div>
   </div>
   `)
 
@@ -21,18 +31,20 @@ function renderMain(town, time, img, event, temp, speed){
 
 function renderRow(time, date, img, temp){
   
-  setInterval(() => {
-    const date = new Date().toLocaleTimeString();
-    const time = document.querySelector('.time');
-    time.textContent = date;
-  }, 1000);
+  const dateToday = date.slice(0, 10);
+  const times = time.slice(11);
+  
   forecast.insertAdjacentHTML('beforeend', `
+  <div class="forecast-wrapper">
   <div class="full-data">
-    <p class="date">${date}</p>
-    <p class="time">${time}</p>
+    <p class="date">${dateToday}</p>
+    <p class="time">${times}</p>
   </div>
-  <img src="${img}" alt="falldown">
-  <p class="forecast-degree">${temp}℃</p>
+  <div class="img-falldown">
+    <img src="https://openweathermap.org/img/wn/${img}@2x.png" alt="falldown">
+  </div>
+  <p class="forecast-degree">${temp.toFixed()}℃</p>
+  </div>
   `)
 }
 
@@ -43,8 +55,11 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q=Hadera&appid=fd6e03c43
     
     renderMain(obj.city.name, obj.list[0].dt_txt, obj.list[0].weather[0].icon, obj.list[0].weather[0].description, obj.list[0].main.temp, obj.list[0].wind.speed);
 
-      for(let i = 0; i < 40; i += 8){
-        
-        renderRow(obj[i].dt_txt, date, obj.list[i].weather[0].icon, obj.list[i].main.temp);
-      }
+    const array = obj.list;
+    console.log(array);
+
+    for(let i = 0; i < 40; i += 8){
+        console.log(array[i]);
+        renderRow(array[i].dt_txt, array[i].dt_txt, array[i].weather[0].icon, array[i].main.temp);
+    }
 });
