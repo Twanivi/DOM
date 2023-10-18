55a5fcabba78fa0a040862af8168ccfd417f274a
Twanivi <139391328+Twanivi@users.noreply.github.com>
2023-10-18 18:53:29 +0300

    Add files via upload

--- hw15/script.js
+++ hw15/script.js
@@ -1,13 +1,51 @@
+// const row = document.querySelector('.row');
+
+// const renderCard = (arrayCards) => { 
+//   // console.log(arrayCards);
+  
+  
+//   const flag = arrayCards.flags.svg;
+//   const nameOf = arrayCards.name.official;
+//   const regions = arrayCards.region;
+//   const populationMln = (+arrayCards.population / 1000000).toFixed(3);
+//   const langs = arrayCards.languages ? Object.values(arrayCards.languages).join(', ') : '-';
+//   const currArray = arrayCards.currencies ? Object.values(arrayCards.currencies) : [];
+//   const currString = currArray.length ? currArray.map(item => item.symbol + ' ' + item.name).join(', ') : '-';
+
+//   row.insertAdjacentHTML('beforeend', `
+//       <div class="col">
+//       <div class="card h-100">
+//         <img src="${flag}" class="card-img-top" alt="${nameOf}">
+//         <div class="card-body">
+//           <h5 class="card-title">${nameOf}</h5>
+//           <p class="card-text">${regions}</p>
+//           <p class="card-text2">👨‍👩‍👦 ${populationMln} млн</p>
+//           <p class="card-text3">🗣 ${langs}</p>
+//           <p class="card-text4">💰 ${currString}</p>
+//         </div>
+//       </div>
+//     </div>
+//       `)
+// }
+
+// fetch('https://restcountries.com/v3.1/all')
+//   .then((response) => response.json())
+//   .then((array) => {
+    
+    
+//     for(let i = 0; i <= array.length; i += 10){
+      
+//       renderCard(array[i]);
+//   } 
+// });
+
 const row = document.querySelector('.row');
 
-const renderCard = (arrayCards) => { 
+const renderCard = (flag, nameOf, regions, population, languages, currencies) => { 
   
-  const flag = arrayCards.flags.svg;
-  const nameOf = arrayCards.name.official;
-  const regions = arrayCards.region;
-  const populationMln = (+arrayCards.population / 1000000).toFixed(3);
-  const langs = arrayCards.languages ? Object.values(arrayCards.languages).join(', ') : '-';
-  const currArray = arrayCards.currencies ? Object.values(arrayCards.currencies) : [];
+  const populationMln = (+population / 1000000).toFixed(3);
+  const langs = languages ? Object.values(languages).join(', ') : '-';
+  const currArray = currencies ? Object.values(currencies) : [];
   const currString = currArray.length ? currArray.map(item => item.symbol + ' ' + item.name).join(', ') : '-';
 
   row.insertAdjacentHTML('beforeend', `
@@ -29,8 +67,9 @@ const renderCard = (arrayCards) => {
 fetch('https://restcountries.com/v3.1/all')
   .then((response) => response.json())
   .then((array) => {
-    
-    for(let i = 0; i <= array.length; i += 10){
-      renderCard(array[i]);
+  
+    const [...arrayNew] = array;
+    for(let i = 0; i <= arrayNew.length; i += 10){
+      renderCard(arrayNew[i].flags.svg, arrayNew[i].name.official, arrayNew[i].region, arrayNew[i].population, arrayNew[i].languages, arrayNew[i].currencies);
   } 
 });
