0adfec7dc589dbeb213d6f195eeda2a27296aff5
Twanivi <139391328+Twanivi@users.noreply.github.com>
2023-10-18 19:50:04 +0300

    Add files via upload

--- hw15/script.js
+++ hw15/script.js
@@ -1,44 +1,3 @@
-// const row = document.querySelector('.row');
-
-// const renderCard = (arrayCards) => { 
-//   // console.log(arrayCards);
-  
-  
-//   const flag = arrayCards.flags.svg;
-//   const nameOf = arrayCards.name.official;
-//   const regions = arrayCards.region;
-//   const populationMln = (+arrayCards.population / 1000000).toFixed(3);
-//   const langs = arrayCards.languages ? Object.values(arrayCards.languages).join(', ') : '-';
-//   const currArray = arrayCards.currencies ? Object.values(arrayCards.currencies) : [];
-//   const currString = currArray.length ? currArray.map(item => item.symbol + ' ' + item.name).join(', ') : '-';
-
-//   row.insertAdjacentHTML('beforeend', `
-//       <div class="col">
-//       <div class="card h-100">
-//         <img src="${flag}" class="card-img-top" alt="${nameOf}">
-//         <div class="card-body">
-//           <h5 class="card-title">${nameOf}</h5>
-//           <p class="card-text">${regions}</p>
-//           <p class="card-text2">👨‍👩‍👦 ${populationMln} млн</p>
-//           <p class="card-text3">🗣 ${langs}</p>
-//           <p class="card-text4">💰 ${currString}</p>
-//         </div>
-//       </div>
-//     </div>
-//       `)
-// }
-
-// fetch('https://restcountries.com/v3.1/all')
-//   .then((response) => response.json())
-//   .then((array) => {
-    
-    
-//     for(let i = 0; i <= array.length; i += 10){
-      
-//       renderCard(array[i]);
-//   } 
-// });
-
 const row = document.querySelector('.row');
 
 const renderCard = (flag, nameOf, regions, population, languages, currencies) => { 
@@ -68,7 +27,9 @@ fetch('https://restcountries.com/v3.1/all')
   .then((response) => response.json())
   .then((array) => {
   
-    for(let i = 0; i <= array.length; i += 10){
+  for(let i = 0; i < array.length; i += 10){
+    
       renderCard(array[i].flags.svg, array[i].name.official, array[i].region, array[i].population, array[i].languages, array[i].currencies);
   } 
+
 });
