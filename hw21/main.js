5b7c219af598f8a7ff653d886c54e3fa0df15998
Twanivi <139391328+Twanivi@users.noreply.github.com>
2023-11-12 20:34:17 +0200

    Delete hw21/main.js

--- hw21/main.js
+++
@@ -1,8 +0,0 @@
-import Button from './button.js';
-import BtnsRendered from './btns.js';
-
-new Button(1, 'Button 1', 'Pit');
-new Button(2, 'Button 2', 'Jane');
-new Button(3, 'Button 3', 'Kyle');
-new BtnsRendered();
-
